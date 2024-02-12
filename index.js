b9d6d31af980c0365b628c2cd69dcbd82099b37f
Buddha Lo <lo_977@yahoo.com>
2024-02-12 13:51:22 -0800

    complete arithemetic lab

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 
 // Write your code here
-let num1 = 2;
-let num2 = 31;
-let multiply = num1 * num2;
-console.log(multiply);
-
-let random = Math.floor(Math.random() * 10) + 1;
-console.log(random);
+let num1 = 31
+let num2 = 2
+const multiply = (num1*num2);
+let random = Math.floor(Math.random() * 100) +1;
 
 let num3 = 14
 let num4 = 5
-let mod = num3 % num4
-console.log(mod);
 
-let max = Math.max(1,4,6,8,20);
-console.log(max);
+let mod = (num3 % num4);
+let nums = [1,2,3,4,5,20];
+ let max = Math.max(...nums);
+ 
